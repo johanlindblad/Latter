743228f99e20ea3748484782b24c0353af91b2af
Johan Lindblad <johanlindblad@gmail.com>
2011-10-01 16:53:12 +0200

    A missing options array to a select field will no longer cause errors.

--- classes/latter/core/field/select.php
+++ classes/latter/core/field/select.php
@@ -11,10 +11,7 @@ class Latter_Core_Field_Select extends Latter_Field
 	{
 		parent::initialize($params);
 
-		if($options = arr::get($params, 'options'))
-		{
-			$this->_options = $options;
-		}
+		$this->_options = arr::get($params, 'options', array());
 		
 		$this->_view_class = 'View_Latter_Field_Select';
 		$this->_template = 'select';
